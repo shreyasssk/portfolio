abedc25e932fe41d04d9193ec013ecd711a928c3
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2021-10-27 00:11:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import NavBar from './components/NavBar';
 const App = () => {
 	return (
 		<div>
+			<div className="alert alert-dark">
+				<h1>This website is still under development</h1>
+				<h4>Please check back again after some time!</h4>
+			</div>
 			<NavBar />
 			<Home />
 		</div>
