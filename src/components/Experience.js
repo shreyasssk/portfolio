a3c70d2cdb97b279973b24eafec07fe17b8a4b68
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2021-10-27 18:54:28 +0530

    Added experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,14 @@
 import './styles.css';
+import data from './experienceData.json';
+
+const ExperienceTemplate = ({ title, desc }) => {
+	return (
+		<div className="card-text">
+			<h3>{title}</h3>
+			<p>{desc}</p>
+		</div>
+	);
+};
 
 const Experience = () => {
 	return (
@@ -6,24 +16,11 @@ const Experience = () => {
 			<div className="card">
 				<div className="card-body">
 					<h2 style={{ color: '#545e69' }}>Experience</h2>
-					<div className="card-text">
-						<h3>Smart India Hackathon</h3>
-						<p>
-							Provide a brief description of your project. Ut enim
-							ad minima veniam, quis nostrum exercitationem ullam
-							corporis suscipit laboriosam, nisi ut aliquid ex ea
-							commodi consequatu.
-						</p>
-					</div>
-					<div className="card-text">
-						<h3>National Level Robotics</h3>
-						<p>
-							Provide a brief description of your project. Ut enim
-							ad minima veniam, quis nostrum exercitationem ullam
-							corporis suscipit laboriosam, nisi ut aliquid ex ea
-							commodi consequatu.
-						</p>
-					</div>
+					{data.map((i) => {
+						return (
+							<ExperienceTemplate title={i.title} desc={i.desc} />
+						);
+					})}
 				</div>
 			</div>
 		</div>
