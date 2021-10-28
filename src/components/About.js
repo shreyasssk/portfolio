473783b66272b389895c2545b827b6c68e9716af
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2021-10-28 14:12:06 +0530

    major updates

--- src/components/About.js
+++ src/components/About.js
@@ -8,9 +8,9 @@ const About = () => {
 					<h2 style={{ color: '#545e69' }}>About Me</h2>
 					<div className="card-text">
 						Reliable, energetic and enthusiastic programmer with a
-						passion for cloud, coding, robotics and graphic design.
-						A hardworking fresher seeking for an opportunity to work
-						in a challenging environment.
+						passion for cloud, coding and robotics. A hardworking
+						fresher seeking for an opportunity to work in a
+						challenging environment.
 					</div>
 				</div>
 			</div>
